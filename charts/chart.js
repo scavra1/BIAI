8c5af6f1f9ee5025f8ae2d32af395f29ffbe462e
lukix <lukix.95@gmail.com>
2017-04-04 19:50:10 +0200

    Add gitignore to /charts

--- charts/chart.js
+++ charts/chart.js
@@ -1,29 +1,31 @@
 (function () {
   var canvas = document.getElementById('tester');
 
-  //var title = 'Execution time with different threads numbers';
+  if(typeof networkErrors === 'undefined') {
+    document.getElementById('tester').innerHTML = "<strong>No data has been found!</strong>";
+  } else {
+    var xdata = new Array(networkErrors.length).fill(0).map((item, index) => index);
 
-  var xdata = new Array(networkErrors.length).fill(0).map((item, index) => index);
+    var trace1 = {
+       name: "Assembly"
+      ,x: xdata
+      ,y: networkErrors
+      ,mode: "lines"
+      ,type: 'scatter'
+    };
 
-  var trace1 = {
-     name: "Assembly"
-    ,x: xdata
-    ,y: networkErrors
-    ,mode: "lines"
-    ,type: 'scatter'
-  };
-
-  var layout = {
-    xaxis: {
-        title: "Iterations"
-        //,range: [ 0.75, 5.25 ]
-    },
-    yaxis: {
-        title: "Error"
-        //,range: [0, 1]
-    }
-    //,title: title
-  };
-  var data = [trace1];
-  Plotly.plot(canvas, data, layout);
+    var layout = {
+      xaxis: {
+          title: "Iterations"
+          //,range: [ 0.75, 5.25 ]
+      },
+      yaxis: {
+          title: "Error"
+          //,range: [0, 1]
+      }
+      //,title: title
+    };
+    var data = [trace1];
+    Plotly.plot(canvas, data, layout);
+  }
 })();
